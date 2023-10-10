dfeab3a4742651101d81e295a822325853c7468d
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-10 22:01:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inlämingsuppgift3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt är din låt i muniter?");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt är din låt i sekunder?");
+            int sekunder = int.Parse(Console.ReadLine());
+            int totaltid = minuter * 60 + sekunder;
+
+            if (totaltid <= 260 && totaltid >= 165)
+            {
+                Console.WriteLine("Din låt får spelas på radion!");
+            }
+            else
+            {
+                Console.WriteLine("Din låt får inte spelas på radion!");
+            }
+        }
+    }
+}
